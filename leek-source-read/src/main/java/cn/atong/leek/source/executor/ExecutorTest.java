4c8b64614ad082a5ece5f2c099356c531ab61f64
atongTs <atongLv@163.com>
2021-07-17 18:00:25 +0800

    feat add soruce md File

---
+++ leek-source-read/src/main/java/cn/atong/leek/source/executor/ExecutorTest.java
@@ -0,0 +1,16 @@
+package cn.atong.leek.source.executor;
+
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+ * @program: leek-atbatis
+ * @description: Executor Test
+ * @author: atong
+ * @create: 2021-07-17 16:44
+ */
+public class ExecutorTest {
+
+    public void init () {
+        SqlSessionFactoryBuilder factoryBuilder = new SqlSessionFactoryBuilder();
+    }
+}
